6244bdda95d0febd28fee21dbb19e4c375e1c49a
Daniel-Ohaio <160250909+Daniel-Ohaio@users.noreply.github.com>
2024-04-14 23:03:12 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,34 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T* StArr;
+    int StTop;
+
+public:
+    TStack() : StArr(new T[size]), StTop(-1) {}
+
+    void Push(T dvalue) {
+        if (StTop != size - 1) {
+            StArr[++StTop] = dvalue;
+        }
+    }
+
+    T pop() {
+        if (StTop != -1) {
+            return StArr[StTop--];
+        } else {
+            return T();
+        }
+    }
+
+    T get() {
+        return (StTop == -1) ? T() : StArr[StTop];
+    }
+
+    bool IsEmptyy() {
+        return StTop == -1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif
